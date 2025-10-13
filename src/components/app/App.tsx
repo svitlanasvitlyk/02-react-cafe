fdd965df17d2d529d4956c87390dae23099a911b
svitlanasvitlyk <65176415+svitlanasvitlyk@users.noreply.github.com>
2025-10-13 17:23:24 +0200

    update step 3

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,36 @@
 import styles from "./App.module.css";
 import CafeInfo from "../cafeInfo/CafeInfo";
+import { useState } from "react";
+import type { Votes, VoteType } from "../../types/votes";
 
-<div className={styles.app}></div>;
+function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  const handleVote = (type: VoteType) => {
+    setVotes((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  return (
+    <>
+      <div className={styles.app}></div>
+
+      <CafeInfo />
+    </>
+  );
+}
 
 export default App;
