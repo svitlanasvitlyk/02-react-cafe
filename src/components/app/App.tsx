08fe1561ffe9b31e3e350ceddbcee9f635521a08
svitlanasvitlyk <65176415+svitlanasvitlyk@users.noreply.github.com>
2025-10-14 11:39:54 +0200

    update app

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,5 +1,6 @@
-import styles from "./App.module.css";
+import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
 import { useState } from "react";
 import type { Votes, VoteType } from "../../types/votes";
 
@@ -23,13 +24,20 @@ function App() {
       bad: 0,
     });
   };
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
 
   return (
-    <>
-      <div className={styles.app}></div>
-
+    <div className={css.app}>
       <CafeInfo />
-    </>
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+    </div>
   );
 }
 
